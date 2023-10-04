db084558f8b53d6e72876f318245fb1f1142a834
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-04 15:52:35 +0200

    ADD: parsetint kilometer and age

--- js/app.js
+++ js/app.js
@@ -4,15 +4,17 @@
 // # Prezzo viaggio utente
 
 // - chiedere all'utente il numero di chilometri che vuole percorrere
-let kilometersNumbers;
-kilometersNumbers = prompt ('Ciao utente, quanti chilometri vuoi percorrere?');
+let kilometersNumbers = prompt('Ciao utente, quanti chilometri vuoi percorrere?');
+kilometersNumbers = parseInt(kilometersNumbers);
 console.log (kilometersNumbers);
 // - chiedere all'utente l'eta
-let userAge;
-userAge = prompt ('Quanti anni hai?');
-console.log (userAge);
+let userAge = prompt ('Quanti anni hai?');
+userAge = parseInt(userAge);
+console.log (typeof kilometersNumbers,typeof userAge);
 // - calcolare il prezzo del biglietto in base ai Km
-
+let priceKilometre;
+priceKilometre = 0,21;
+console.log (priceKilometre); 
 //     - SE l'utente è < 18 anni 
 //         - applicare lo sconto del 20%
 //     - ALTRIMENTI SE è > 65 anni 
