f5124823f227a1e660bc5c5a115bf0551feb1931
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-04 18:02:04 +0200

    FIX: README and comments JS

--- js/app.js
+++ js/app.js
@@ -6,45 +6,45 @@
 // - chiedere all'utente il numero di chilometri che vuole percorrere
 let kilometersNumbers = prompt('Ciao utente, quanti chilometri vuoi percorrere?');
 kilometersNumbers = parseInt(kilometersNumbers);
-console.log('kilometri user')
-console.log(kilometersNumbers);
+console.log('kilometri user', kilometersNumbers);
+
 // - chiedere all'utente l'eta
 let userAge = prompt('Quanti anni hai?');
 userAge = parseInt(userAge);
-console.log('Age user')
-console.log (userAge)
+console.log('Age user', userAge);
 // console.log (typeof kilometersNumbers,typeof userAge);
 
 // - calcolare il prezzo del biglietto in base ai Km
 let priceBaseKilometre;
 priceBaseKilometre = 0.21;
-console.log ('prezzo base kilometro')
-console.log(priceBaseKilometre);
+console.log ('prezzo base kilometro', priceBaseKilometre);
 // console.log (typeof priceBaseKilometre);
 
+// - calcolare il prezzo totale senza
 let totalPrice = priceBaseKilometre * kilometersNumbers;
-console.log('prezzo totale')
-console.log( totalPrice);
+console.log('prezzo totale',totalPrice);
 
-
+// - in base alla fascia d'eta ci sono diversi sconti
 // discount 
 let discount;
-//     - SE l'utente è < 18 anni
-if (userAge < 18) {
-//         - applicare lo sconto del 20%
-discount = totalPrice * (20 / 100);
-console.log('sconto minori')
-console.log(discount)
-
-}
-//     - ALTRIMENTI SE è > 65 anni
-else if (userAge > 65) {
-//         - applicare lo sconto del 40% 
-discount = totalPrice * (40 / 100);
-console.log('sconto over 65')
-console.log(discount)
-
-}
+    //     - SE l'utente è < 18 anni
+    if (userAge < 18) {
+    //         - applicare lo sconto del 20%
+    discount = totalPrice * (20 / 100);
+    console.log('sconto minori',discount);
+    }
+    //     - ALTRIMENTI SE è > 65 anni
+    else if (userAge > 65) {
+    //         - applicare lo sconto del 40% 
+    discount = totalPrice * (40 / 100);
+    console.log('sconto over 65',discount);
+    }
+    // - ALTRIMENTI
+    else {
+        totalPrice;
+    //          - non si hanno sconti
+        console.log('niente sconto');
+    }
 
 let totalPriceDiscount = totalPrice - discount;
 console.log ('prezzo totale sconto');
@@ -53,8 +53,15 @@ console.log ('prezzo totale sconto');
 totalPriceDiscount = totalPriceDiscount.toFixed(2);
 console.log(totalPriceDiscount);
 
+// - stampare il prezzo finale
+//      - recuperare dal DOM un elemento in base id:
 let price = totalPriceDiscount
-
+//         - dichiarare una variabile e tramite getelementbyid assegnarvi un valore
 const priceDomElement = document.getElementById('price');
+//     - modificare l'inner con il valore di prince
 priceDomElement.innerHTML = price;
+
+
+
+
 
--- readme/readme.md
+++ readme/readme.md
@@ -3,9 +3,15 @@
 - chiedere all'utente il numero di chilometri che vuole percorrere
 
 - chiedere all'utente l'eta
-
-- calcolare il prezzo del biglietto in base ai Km
+- calcolare il prezzo totale senza
+- in base alla fascia d'eta ci sono diversi sconti
     - SE l'utente è < 18 anni 
         - applicare lo sconto del 20%
     - ALTRIMENTI SE è > 65 anni 
         - applicare lo sconto del 40%
+    - ALTRIMENTI
+        - non si hanno sconti
+- stampare il prezzo finale
+     - recuperare dal DOM un elemento in base id:
+        - dichiarare una variabile e tramite getelementbyid assegnarvi un valore
+    - modificare l'inner con il valore di prince
